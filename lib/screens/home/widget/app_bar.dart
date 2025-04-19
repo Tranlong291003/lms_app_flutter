58c52b8404475cdf3b4727e73f916274c531c326
Trần Khánh Long <tranlong291003@gmail.com>
2025-04-20 00:42:28 +0700

    change avt

--- lib/screens/home/widget/app_bar.dart
+++ lib/screens/home/widget/app_bar.dart
@@ -28,7 +28,7 @@ AppBar AppBarHome(BuildContext context, String title) {
           children: [
             CircleAvatar(
               backgroundImage: NetworkImage(
-                'https://www.example.com/your_image.jpg', // Thay đổi đường dẫn ảnh
+                'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=', // Thay đổi đường dẫn ảnh
               ),
             ),
             SizedBox(width: 10),
